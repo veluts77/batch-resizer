3cf7e1534cfb26d9cc93114e7af4855eec41a050
Vladimir Lutsenko <veluts77@gmail.com>
2019-05-05 23:47:41 +0300

    #165805922 It is possible to specify image filtering parameters

--- batch_resize_images.rb
+++ batch_resize_images.rb
@@ -9,9 +9,9 @@ def gets_with_default(message, default)
   print message
   print " [#{default}]" unless default.nil?
   print ': '
-  result = gets
-  result = default if result.chomp.size.zero?
-  result
+  result = gets.chomp
+  result = default if result.size.zero?
+  result.to_i
 end
 
 # Selection of images that can be downsized
@@ -24,14 +24,22 @@ class ImagesSelection
 
   def input
     puts 'Image selection section.'
-    msg = 'Smaller size should be more than (in pixels) '
+    msg = 'Smaller dimension should be more than (in pixels) '
     @threshold_size = gets_with_default msg, 2200
   end
 
   def apply
+    print 'Selecting appropriate files... '
     Dir.foreach('.') do |filename|
-      @selection.push(filename) if jpeg_image? filename
+      @selection.push(filename) if accepted_image? filename
     end
+    puts 'done.'
+  end
+
+  def show_current
+    puts "The following files are selected (#{@selection.size} in total):"
+    each { |filename| print "#{filename} " }
+    puts
   end
 
   def each(&block)
@@ -40,13 +48,25 @@ class ImagesSelection
 
   private
 
+  def accepted_image?(filename)
+    (jpeg_image? filename) && (smaller_dimension_exceeds_threshold? filename)
+  end
+
   def jpeg_image?(filename)
     is_jpeg = (filename.end_with? 'jpg') || (filename.end_with? 'jpeg')
     is_jpeg
+  end
+
+  def smaller_dimension_exceeds_threshold?(filename)
+    image = Magick::Image.read(filename).first
+    min_dimension = [image.columns, image.rows].min
+    exceeds = min_dimension > @threshold_size
+    exceeds
   end
 end
 
 selection = ImagesSelection.new
 selection.input
 selection.apply
+selection.show_current
 selection.each { |filename| puts filename }
